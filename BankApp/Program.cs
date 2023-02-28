d2841a35d8c7b0dd5686fcb3e165c1b0447cddbf
Cavidan Shushayev <csusayev19@gmail.com>
2023-02-28 20:45:46 +0400

    commit for Rustem teacher

--- BankApp/Program.cs
+++ BankApp/Program.cs
@@ -32,69 +32,109 @@ namespace BankApp
                 int ayliq_odenis=0;
                 int odenilecek_mebleg=0;
             breakpoint2:
-                Console.WriteLine("1-kredit goturmek,2-kredit odemek ,3 hesabat");
+                Console.WriteLine("1-kredit goturmek,2-kredit odemek ,3 hesabat,4 cixhis");
                 int emeliyyat = Convert.ToInt32(Console.ReadLine());
                 switch (emeliyyat)
                 {
                     case 1:
-                        Console.WriteLine("ne qeder kredit goturmek isteyirsiniz?");
-                        mebleg = Convert.ToInt32(Console.ReadLine());
-                        Console.WriteLine("nece ayliq kredit goturmek isteyirsiniz?");
-                        ay = Convert.ToInt32(Console.ReadLine());
-                        if (ay <= 12) { 
-                        odenilecek_mebleg = mebleg + (mebleg * 12 / 100); }
+                        if (odenilecek_mebleg == 0)
+                        {
+                            Console.WriteLine("ne qeder kredit goturmek isteyirsiniz?");
+                            mebleg = Convert.ToInt32(Console.ReadLine());
+                            Console.WriteLine("nece ayliq kredit goturmek isteyirsiniz?");
+                            ay = Convert.ToInt32(Console.ReadLine());
+                            if (ay <= 12)
+                            {
+                                odenilecek_mebleg = mebleg + (mebleg * 12 / 100);
+                            }
+                            else
+                            {
+                                odenilecek_mebleg = mebleg + (mebleg * 18 / 100);
+                            }
+                            Console.WriteLine("odenlicek mebleg=" + odenilecek_mebleg);
+                            ayliq_odenis = odenilecek_mebleg / ay;
+                            Console.WriteLine("ayliq odenish=" + ayliq_odenis);
+                            Console.WriteLine("davam etmek isteyirsinizmi? 1.beli ,2 xeyr");
+                            int emeli = Convert.ToInt32(Console.ReadLine());
+                            if (emeli == 1)
+                            {
+                                Console.WriteLine("xeyirli olsun ,kreditiniz tesdiqlendi.Banka odenilecek umumi mebleg" + odenilecek_mebleg);
+                                goto breakpoint2;
+                            }
+                            else if (emeli == 2)
+                            {
+                                Console.WriteLine("teesuf ki kredit gotrulmedi");
+                                goto breakpoint3;
+                            }
+                            else
+                            {
+                                Console.WriteLine("teesuf ki bele emeliyyat movcud deyil");
+                                goto breakpoint2;
+                            }
+                        }
                         else
                         {
-                            odenilecek_mebleg = mebleg + (mebleg * 18 / 100);
-                        }
-                        Console.WriteLine("odenlicek mebleg=" + odenilecek_mebleg);
-                        ayliq_odenis = odenilecek_mebleg / ay;
-                        Console.WriteLine("ayliq odenish=" + ayliq_odenis);
-                        Console.WriteLine("davam etmek isteyirsinizmi? 1.beli ,2 xeyr");
-                        int emeli = Convert.ToInt32(Console.ReadLine());
-                        if (emeli == 1)
-                        {
-                            Console.WriteLine("xeyirli olsun ,kreditiniz tesdiqlendi.Banka odenilecek umumi mebleg" + odenilecek_mebleg);
-                            goto breakpoint2;
-                        }else if(emeli == 2)
-                        {
-                            Console.WriteLine("teesuf ki kredit gotrulmedi" );
-                            goto breakpoint3;
-                        }else
-                        {
-                            Console.WriteLine("teesuf ki bele emeliyyat movcud deyil");
-                            goto breakpoint2;
+                            Console.WriteLine("tessuf ki sizin banka borcunuz var,yeni kredit goturmek ucun hazirki krediti odeyin");
+                            Console.WriteLine("basqa emeliyyatdan istifade etmek isteyirsinizmi? 1.beli ,2.xeyre");
+                            int emell = Convert.ToInt32(Console.ReadLine());
+                            if (emell == 1)
+                            {
+                                goto breakpoint2;
+                            }
+                            else
+                            {
+                                break;
+                            }
                         }
 
                         break;
 
                     case 2:
-                        Console.WriteLine("odenilecek mebleg"+ayliq_odenis +"azn dir . odemek isteyirsinizmi? 1.beli ,2 xeyr");
+                    breakpoint4:
+                        Console.WriteLine("ayliq odenish "+ayliq_odenis +"azn dir . neche ayi odemek isteyirsiniz?  ");
+                        int aysay = Convert.ToInt32(Console.ReadLine());
 
-                        int emel = Convert.ToInt32(Console.ReadLine());
-                        if (emel == 1)
-                        {   
-                            Console.WriteLine("xeyirli olsun ,kreditinizi odedeniz");
-                            odenilecek_mebleg = odenilecek_mebleg - ayliq_odenis;
-                            Console.WriteLine("qaliq kredit=" + odenilecek_mebleg + "azndir");
-                        }
-                        else if (emel == 2)
-                        {
-                            Console.WriteLine("teesuf ki kredit odenmedi");
+                        if (aysay <= ay)
+                        {    
+                            Console.WriteLine(aysay + " -ayliq odenish " + aysay * ayliq_odenis + "azndir");
+                            Console.WriteLine("odemek isteyirsinizmi? 1.beli ,2 xeyr");
+                            int emel = Convert.ToInt32(Console.ReadLine());
+                            if (emel == 1)
+                            {
+                                Console.WriteLine("xeyirli olsun ,kreditinizi odedeniz");
+                                odenilecek_mebleg = odenilecek_mebleg - (ayliq_odenis * aysay);
+                                Console.WriteLine("qaliq kredit=" + odenilecek_mebleg + "azndir");
+                            }
+                            else if (emel == 2)
+                            {
+                                Console.WriteLine("teesuf ki kredit odenmedi");
+                                goto breakpoint2;
+                            }
+                            else
+                            {
+                                Console.WriteLine("teesuf ki bele emeliyyat movcud deyil");
+                            }
                             goto breakpoint2;
-                        }
-                        else
+                        }else
                         {
-                            Console.WriteLine("teesuf ki bele emeliyyat movcud deyil");
+                            Console.WriteLine("zehmet olmasa odemek istediyiniz ayi duzgun daxil edin!");
+                            goto breakpoint4;
                         }
-                        goto breakpoint2;
-
                         break;
 
                     case 3:
                         Console.WriteLine("qaliq mebleg=" + odenilecek_mebleg);
                         goto breakpoint2;
                         break;
+                    case 4:
+                        Console.WriteLine("eminsinizmi cixmaq isteyirsinizmi?");
+                        break;
+                    default :
+                        Console.WriteLine("zehmet olmasa emeliyyati duzgun daxil edin");
+                        goto breakpoint2;
+                        break;
+
+
 
                 }
 
